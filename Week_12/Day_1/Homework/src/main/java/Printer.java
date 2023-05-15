2dc9e6ed06d02a45ec2de0be34bf7e3b0f7edc79
McMuds <clairemcmurdo@gmail.com>
2023-05-15 17:16:03 +0100

    Added Toner - as a mandatory requirement.

--- Week_12/Day_1/Homework/src/main/java/Printer.java
+++ Week_12/Day_1/Homework/src/main/java/Printer.java
@@ -1,22 +1,32 @@
 public class Printer {
     private int sheetCount;
+    private int tonerCount;
 
     public Printer(){
         this.sheetCount = 0;
+        this.tonerCount = 0;
     }
 
     public int getSheetCount(){
         return this.sheetCount;
     }
 
+    public int getTonerCount(){
+        return this.tonerCount;
+    }
     public void setSheetCount(int numOfSheets){
         this.sheetCount = numOfSheets;
     }
 
+    public void setTonerCount(int tonerCount) {
+        this.tonerCount = tonerCount;
+    }
+
     public void print(int copies, int sheets){
         int required = copies * sheets;
-        if (required <= this.sheetCount) {
+        if (required <= this.sheetCount && required <= this.tonerCount) {
             this.sheetCount -= required;
+            this.tonerCount -= required;
         }
     }
 }
--- Week_12/Day_1/Homework/src/test/java/PrinterTest.java
+++ Week_12/Day_1/Homework/src/test/java/PrinterTest.java
@@ -25,13 +25,39 @@ public class PrinterTest {
     @Test
     public void printerCanPrint(){
         printer.setSheetCount(15);
+        printer.setTonerCount(15);
         printer.print(3,5);
         assertEquals(0, printer.getSheetCount());
     }
     @Test
     public void printerCannotPrint(){
         printer.setSheetCount(10);
+        printer.setTonerCount(11);
         printer.print(3,5);
         assertEquals(10, printer.getSheetCount());
+    }
+
+    @Test
+    public void hasToner(){
+        printer.setSheetCount(15);
+        printer.setTonerCount(10);
+        assertEquals(10, printer.getTonerCount());
+    }
+
+    @Test
+    public void tonerReduces(){
+        printer.setSheetCount(15);
+        printer.setTonerCount(10);
+        printer.print(2,4);
+        assertEquals(2,printer.getTonerCount());
+    }
+
+    @Test
+    public void printerCannotPrintBecauseNoToner(){
+        printer.setSheetCount(16);
+        printer.setTonerCount(10);
+        printer.print(3,4);
+        assertEquals(16,printer.getSheetCount());
+        assertEquals(10, printer.getTonerCount());
     }
 }
