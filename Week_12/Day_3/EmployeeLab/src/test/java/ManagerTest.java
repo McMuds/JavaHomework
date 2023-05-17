a02b002ad073ead8a6a52cca6aee5ebb53bc271a
McMuds <clairemcmurdo@gmail.com>
2023-05-17 16:12:53 +0100

    Third test passing for manager

--- Week_12/Day_3/EmployeeLab/src/test/java/ManagerTest.java
+++ Week_12/Day_3/EmployeeLab/src/test/java/ManagerTest.java
@@ -11,26 +11,28 @@ public class ManagerTest {
     public void before(){
         manager = new Manager("Eric", "NZ435623D", 69000.00, "CorpDev");
     }
-
     @Test
     public void hasDepartment(){
         assertEquals("CorpDev", manager.getDeptName());
     }
-
     @Test
     public void hasSalary(){
         assertEquals(69000.00, manager.getSalary(), 0.0);
     }
-
     @Test
     public void hasName(){
         assertEquals("Eric", manager.getName());
+    }    @Test
+    public void hasNINO(){
+        assertEquals("NZ435623D", manager.getNINO());
     }
     @Test
     public void canRaiseSalary(){
         manager.raiseSalary(1500.00);
         assertEquals(70500.00, manager.getSalary(), 0.0);
     }
-
-
+    @Test
+    public void canRetrieveRaise(){
+        assertEquals(690.00, manager.payBonus(), 0.0);
+    }
 }
