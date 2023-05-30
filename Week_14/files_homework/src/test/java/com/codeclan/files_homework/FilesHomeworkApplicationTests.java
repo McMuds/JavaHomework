cd8553010cb11fcbe0061292b1a4e1b78989c1cb
McMuds <clairemcmurdo@gmail.com>
2023-05-30 14:53:22 +0100

    Initial Commit

---
+++ Week_14/files_homework/src/test/java/com/codeclan/files_homework/FilesHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.files_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
